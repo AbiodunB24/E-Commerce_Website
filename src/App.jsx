c997d0961e359963511ed5efb5ab9cc893af770b
Babatunde Balogun <balogunbabatunde42@gmail.com>
2024-03-26 23:28:36 +0100

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ import { useEffect } from "react";
 import { useStateValue } from "./StateProvider";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
+import Payment from "./payment/Payment"
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe("pk_test_51OyUoIJeoJTrziWF9tjqtUrlFLgZvLENeQEupmnahFF13ByNrAtQKEvh6uMzlBpVLTjK8eG6IYZNPLYMOxgkxw4T00QJX7fky3");
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
@@ -47,6 +51,7 @@ function App() {
       <Route path="/checkout" element={<div><Header /> <Checkout/> </div> }/>
         <Route path="/" element={<div><Header /> <Home/> </div> }/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/payment" element={<div><Header/> <Elements stripe={promise}> <Payment/> </Elements> </div>} />
       </Routes>
      
     </div>
